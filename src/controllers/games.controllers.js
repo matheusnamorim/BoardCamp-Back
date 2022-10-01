4b0b55929ff1b919c34db542fa5345e71881468a
Amorim <matheusnavesamorim@gmail.com>
2022-10-01 12:01:36 -0300

    Feat: listando games

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -11,4 +11,11 @@ const registerGames  = (req, res) => {
     return res.status(STATUS_CODE.CREATED).send(MESSAGES.CREATED);
 };
 
-export {registerGames};
+const ListGames = async (req, res) => {
+    const list = await connection.query(`
+    SELECT games.id, games.name, games.image,  games."stockTotal", games."categoryId", games."pricePerDay", categories.name AS "categoryName"   FROM games JOIN categories ON games."categoryId" = categories.id;`
+    );
+    return res.status(STATUS_CODE.OK).send(list.rows);
+};
+
+export {registerGames, ListGames};
--- src/routers/games.routers.js
+++ src/routers/games.routers.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { registerGames } from "../controllers/games.controllers.js";
+import { ListGames, registerGames } from "../controllers/games.controllers.js";
 import { validateGames } from "../middlewares/games.middlewares.js";
 
 const router = express.Router();
 
 router.post('/games', validateGames, registerGames);
-
+router.get('/games', ListGames);
 
 export default router;
